bc7060936e61dfae0f07ceb8e07aae0f1746cf50
Osama-JS <osama.samomy@gmail.com>
2025-09-25 09:57:22 +0300

    AppDbContext update

--- Program.cs
+++ Program.cs
@@ -1,5 +1,9 @@
+using Emtias.Data;
+using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
